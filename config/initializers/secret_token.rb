bee002cd4df29592cd71e0255cf3606d0bc6a5c2
obi-a <obioraakubue@yahoo.com>
2013-05-10 19:36:10 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LocatePics::Application.config.secret_token = '170c94de65d810cd1078c5df687ce4312d0d1105a242e969daa6a9ea09e4042bd9bed469f64491b5bffd44d646c4e62fc773e2d441611a5fb44effa670175787'
