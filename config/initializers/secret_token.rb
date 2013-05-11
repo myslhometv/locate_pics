ee8a4af0eec54dec07bc094584c1e67ba4a26153
obi-a <obioraakubue@yahoo.com>
2013-05-11 04:57:19 -0400

    generate new secret - and hide from repo

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-LocatePics::Application.config.secret_token = '170c94de65d810cd1078c5df687ce4312d0d1105a242e969daa6a9ea09e4042bd9bed469f64491b5bffd44d646c4e62fc773e2d441611a5fb44effa670175787'
+LocatePics::Application.config.secret_token = 'generate a new one with rake secret'
